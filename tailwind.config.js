390f45f417a7dad1cc26948498653857efa44f96
RIYAYADAV07 <tinkleyadav19@gmail.com>
2021-02-23 18:28:05 +0530

    desktop ready

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  purge: [],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {},
+  },
+  variants: {
+  },
+  plugins: [],
+};
+// "./**/*.html"
